435e4816185414863ce7d272d243ed0a72599638
nShahaf <shahafnoked@gmail.com>
2024-07-08 15:18:10 +0300

    contactDetails

--- pages/ContactDetails.jsx
+++ pages/ContactDetails.jsx
@@ -1,13 +1,30 @@
-export function ContactDetails({contact}){
-    const demoContact = {name:'John',familyName:'Snow',email:'JohnSnow@gmail.com'}
-    const fullname = `${demoContact.name} ${demoContact.familyName}`
+const { useParams, Link } = ReactRouterDOM
+const { useEffect, useState } = React
 
+import { contactService } from "../services/contact.service.js"
 
+export function ContactDetails(){
+    // const demoContact = {name:'John',familyName:'Snow',email:'JohnSnow@gmail.com'}
+    // const fullname = `${demoContact.name} ${demoContact.familyName}`
+    const [contact, setContact] = useState(null)
+    const { contactId } = useParams()
+
+    useEffect(() => {
+        contactService.get(contactId)
+            .then(contact  => setContact(contact))
+    }, [contactId])
+    
+    
+    if (!contact) return <div>Loading...</div>
+    const fullname = `${contact.name} ${contact.familyName}`
     return (
-        <div>
+        <section className="contact-details">
             <h1>Contact details</h1>
-            <h4>{`${fullname}`}</h4>
-            <h4>{demoContact.email}</h4>
-        </div>
+            <h4 className="name">{`${fullname}`}</h4>
+            <h4 className="email">{demoContact.email}</h4>
+            <button ><Link to="/">Back</Link></button>
+        </section>
     )
 }
+
+{/* <Route path="/contact/:contactId" element={<ContactDetails />} /> */}
